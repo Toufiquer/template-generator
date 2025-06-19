22c9c39fbb5ce4bcaa88fc18da319079b7f743d2
Toufiquer <toufiquerabc@gmail.com>
2025-06-20 01:12:03 +0600

    done @ moon button toggle

--- src/components/common/mode-toggle.tsx
+++ src/components/common/mode-toggle.tsx
@@ -4,8 +4,6 @@ import * as React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/ui/button';
-
 export function ModeToggle() {
   const { setTheme, theme, resolvedTheme } = useTheme();
 
@@ -24,12 +22,12 @@ export function ModeToggle() {
   const isDark = (resolvedTheme || theme) === 'light';
 
   return (
-    <Button variant="outline" size="icon" onClick={handleToggle} className="cursor-pointer">
+    <div onClick={handleToggle} className="cursor-pointer">
       {isDark ? (
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       ) : (
         <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       )}
-    </Button>
+    </div>
   );
 }
