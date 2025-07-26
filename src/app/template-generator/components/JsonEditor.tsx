8fba0289ad6efbfc9f058674501181ab371243ce
Toufiquer <toufiquerabc@gmail.com>
2025-07-27 01:33:06 +0600

    done @ template generator json file

--- src/app/template-generator/components/JsonEditor.tsx
+++ src/app/template-generator/components/JsonEditor.tsx
@@ -60,9 +60,6 @@ const JsonEditor: React.FC = () => {
         if (error) setError('')
     }
 
-    const handleFormat = () => {
-        console.log('json : ', jsonInput)
-    }
     const handleGenerate = () => {}
 
     return (
@@ -104,12 +101,7 @@ const JsonEditor: React.FC = () => {
                         >
                             {isLoading ? 'Saving...' : 'Save'}
                         </Button>
-                        <Button
-                            onClick={handleFormat}
-                            className="px-6 py-2 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
-                        >
-                            Formet
-                        </Button>
+
                         <Button
                             onClick={handleGenerate}
                             className="px-6 py-2 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
