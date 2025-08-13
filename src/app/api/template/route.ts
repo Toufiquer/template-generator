9955015151bbf844d20d24e450753a4b9deebab7
tpcwebbd-ops <tpc.web.bd@example.com>
2025-08-13 14:38:14 +0600

    fix bug

--- src/app/api/template/route.ts
+++ src/app/api/template/route.ts
@@ -1,11 +1,14 @@
 // app/api/generate-model/route.ts
 import { NextRequest, NextResponse } from 'next/server'
+import generateApi from './components/generate-api/main'
 
 export async function POST(request: NextRequest) {
     try {
         // //  !  create api
         // const { data } = await request.json()
         // console.log('Data : ', data)
+        generateApi(request)
+
         return NextResponse.json({ message: 'file found' }, { status: 200 })
     } catch (e) {
         console.log(e)
