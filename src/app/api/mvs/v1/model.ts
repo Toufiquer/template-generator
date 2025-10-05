af2afb88734c1350846edde119a4a456a2783cd0
tpcwebbd-ops <tpc.web.bd@example.com>
2025-10-05 19:46:45 +0600

    remove all others items

--- src/app/api/mvs/v1/model.ts
+++
@@ -1,7 +0,0 @@
-import mongoose, { Schema } from 'mongoose'
-
-const mvSchema = new Schema({
-    "title": { type: String }
-}, { timestamps: true })
-
-export default mongoose.models.Mv || mongoose.model('Mv', mvSchema)
